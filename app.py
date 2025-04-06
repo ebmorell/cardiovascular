fd050d0a7b3929c7bfd7214e2e231a3a67cf8072
ebmorell <ebm.hgurs@gmail.com>
2025-04-06 19:11:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 🔗 DESCARGAR MODELO Y VARIABLES DESDE GOOGLE DRIVE
-model_url = "https://drive.google.com/file/d/1W1YP0NbDCC78VGcKXwvxXWWqQGQ-13v1/view?usp=drive_link"
+model_url = "https://drive.google.com/file/d/1eO3KBHMjxtXUVOdtDY-Z2z8kAH3MEgeH/view?usp=sharing"
 features_url = "https://drive.google.com/file/d/1J14Tgotjiszcmu5ovY4N4Rsh911nluyz/view?usp=drive_link"
 
 if not os.path.exists("rsf_model.pkl"):
